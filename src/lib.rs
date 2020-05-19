07e667f9a9529046e3982a8f43e7b82972ccb04e
Itiua <ValeriyBushenev@gmail.com>
2020-05-19 20:37:21 +0300

    set main types as pub

--- src/lib.rs
+++ src/lib.rs
@@ -14,21 +14,21 @@ pub mod xapian {
     extern "C" {
         include!("xapian-bind.h");
 
-        type Database;
+        pub type Database;
         pub fn new_database(err: &mut i8) -> UniquePtr<Database>;
         pub fn new_database_with_path(path: &str, db_type: i8, err: &mut i8) -> UniquePtr<Database>;
         pub fn database_reopen(db: &mut Database, err: &mut i8);
 
-        type Stem;
+        pub type Stem;
         pub fn new_stem(lang: &str, err: &mut i8) -> UniquePtr<Stem>;
 
-        type WritableDatabase;
+        pub type WritableDatabase;
         pub fn new_writable_database_with_path(path: &str, action: i8, db_type: i8, err: &mut i8) -> UniquePtr<WritableDatabase>;
         pub fn commit(db: &mut WritableDatabase, err: &mut i8);
         pub fn replace_document(db: &mut WritableDatabase, unique_term: &str, doc: &mut Document, err: &mut i8) -> u32;
         pub fn delete_document(db: &mut WritableDatabase, unique_term: &str, doc: &mut Document, err: &mut i8);
 
-        type TermGenerator;
+        pub type TermGenerator;
         pub fn new_termgenerator(err: &mut i8) -> UniquePtr<TermGenerator>;
         pub fn set_stemmer(tg: &mut TermGenerator, stem: &mut Stem, err: &mut i8);
         pub fn set_document(tg: &mut TermGenerator, doc: &mut Document, err: &mut i8);
@@ -38,7 +38,7 @@ pub mod xapian {
         pub fn index_float(tg: &mut TermGenerator, data: f32, err: &mut i8);
         pub fn index_double(tg: &mut TermGenerator, data: f64, err: &mut i8);
 
-        type Document;
+        pub type Document;
         pub fn new_document(err: &mut i8) -> UniquePtr<Document>;
         pub fn add_string(doc: &mut Document, slot: u32, data: &str, err: &mut i8);
         pub fn add_int(doc: &mut Document, slot: u32, data: i32, err: &mut i8);
